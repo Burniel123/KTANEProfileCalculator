5e6e5a1617283320924bf3c9e31c0c31fd7f3339
Daniel Burton <dgburton12@gmail.com>
2020-06-18 20:21:08 +0100

    Add basic program files.

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * Holds all fundamental program logic relating to the KTANE Profile Calculator, such as startup.
+ *
+ * @author Daniel Burton
+ */
+public class Main
+{
+    /**
+     * Main method for the KTANE Profile Calculator.
+     * @param args - list of command line arguments for the program. These may include:
+     *             Flags to indicate which operations to use.
+     *             References to files which should be used in the calculation.
+     *             Desired name for the completed profile.
+     */
+    public static void main(String[] args)
+    {
+
+    }
+}
